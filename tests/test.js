3e4aa69efc1eee15dd539bb54e81ce8566128ffc
jacksonBispo <jackson.novaes@live.com>
2024-03-10 00:14:03 -0300

    add repo in remote

---
+++ tests/test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+const soma = require('../app');
+
+describe('Testes da função soma', function() {
+  it('Deve retornar a soma de dois números', function() {
+    assert.strictEqual(soma(1, 2), 3);
+  });
+});
