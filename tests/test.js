89ecd4f7fc2017f9024dc51526b9bfd142d1ac17
jacksonBispo <jackson.novaes@live.com>
2024-03-11 16:04:07 -0300

    forcing erro in tests

--- tests/test.js
+++ tests/test.js
@@ -3,6 +3,6 @@ const soma = require('../app');
 
 describe('Testes da função soma', function() {
   it('Deve retornar a soma de dois números', function() {
-    assert.strictEqual(soma(1, 2), 3);
+    assert.strictEqual(soma(1, 2), 4);
   });
 });
